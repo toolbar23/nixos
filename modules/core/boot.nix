1247e6e39fa34d06c91db9ab3d85fb0a149096f0
Paul Mallach <paul.mallach@gmail.com>
2025-11-25 21:14:42 +0100

    Switch bootloader to grub EFI (avoid bootctl issues)

--- modules/core/boot.nix
+++ modules/core/boot.nix
@@ -1,5 +1,10 @@
 { lib, ... }: {
-  boot.loader.systemd-boot.enable = lib.mkDefault true;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   boot.initrd.systemd.enable = true;
